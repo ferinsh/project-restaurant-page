a8a09a2c77da0f6297376a9be080644b5d7f3182
Ferin Sharaf <146843308+ferinsh@users.noreply.github.com>
2023-11-28 13:16:15 +0530

    1.0.2

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import '../src/style.css';
+import defaultBodyStyle from './defaultStyle'; './defaultStyle.js';
+import Background from '../src/background.jpg';
+
+const defaultFontFamily = "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+const defaultColor = "white";
+console.log("HI MANE");
+
+
+
+defaultBodyStyle(defaultFontFamily);
+
